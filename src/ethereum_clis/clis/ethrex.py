ead50569d07ab59fc9fddce549776e761333dfd3
fmoletta <fedemoletta@hotmail.com>
2025-04-29 15:41:10 -0300

    Fix regex

--- src/ethereum_clis/clis/ethrex.py
+++ src/ethereum_clis/clis/ethrex.py
@@ -10,7 +10,6 @@ class EthrexExceptionMapper(ExceptionMapper):
         TransactionException.SENDER_NOT_EOA: (
             "reject transactions from senders with deployed code"
         ),
-        TransactionException.INSUFFICIENT_ACCOUNT_FUNDS: "lack of funds",
         TransactionException.INITCODE_SIZE_EXCEEDED: "create initcode size limit",
         TransactionException.INSUFFICIENT_MAX_FEE_PER_GAS: "gas price is less than basefee",
         TransactionException.INSUFFICIENT_MAX_FEE_PER_BLOB_GAS: (
@@ -19,7 +18,7 @@ class EthrexExceptionMapper(ExceptionMapper):
         TransactionException.PRIORITY_GREATER_THAN_MAX_FEE_PER_GAS: (
             "priority fee is greater than max fee"
         ),
-        TransactionException.TYPE_3_TX_INVALID_BLOB_VERSIONED_HASH: "Exceeded MAX_BLOB_GAS_PER_BLOCK",
+        TransactionException.TYPE_3_TX_MAX_BLOB_GAS_ALLOWANCE_EXCEEDED: "Exceeded MAX_BLOB_GAS_PER_BLOCK",
         TransactionException.TYPE_4_EMPTY_AUTHORIZATION_LIST: "empty authorization list",
         TransactionException.TYPE_4_TX_CONTRACT_CREATION: "unexpected length",
         TransactionException.TYPE_4_TX_PRE_FORK: (
@@ -46,7 +45,12 @@ class EthrexExceptionMapper(ExceptionMapper):
         TransactionException.TYPE_3_TX_ZERO_BLOBS: (
             r"blob transactions present in pre-cancun payload|empty blobs"
         ),
-        BlockException.SYSTEM_CONTRACT_CALL_FAILED: (r"failed to apply .* requests contract call"),
+        TransactionException.INSUFFICIENT_ACCOUNT_FUNDS: (
+            r"Invalid Transaction: lack of funds (\d+) for max fee (\d+)"
+        ),
+        BlockException.SYSTEM_CONTRACT_CALL_FAILED: (
+            r"failed to apply .* requests contract call"
+        ),
         BlockException.INCORRECT_BLOB_GAS_USED: (
             r"Blob gas used doesn't match value in header"
         ),
@@ -54,7 +58,7 @@ class EthrexExceptionMapper(ExceptionMapper):
             r"excess blob gas \d+ is not a multiple of blob gas per blob|invalid excess blob gas"
         ),
         BlockException.RLP_STRUCTURES_ENCODING: (
-            r"Error decoding field 'd+' of type d+"),
+            r"Error decoding field `\D+` of type \w+"),
         BlockException.INCORRECT_EXCESS_BLOB_GAS: (
             r".* Excess blob gas is incorrect"
         )
