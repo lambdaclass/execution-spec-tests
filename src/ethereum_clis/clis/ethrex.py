cbcf49d0271d544ea86c797ae17a65761fc5d8f3
fmoletta <fedemoletta@hotmail.com>
2025-05-05 15:22:24 -0300

    update mappings (levm)

--- src/ethereum_clis/clis/ethrex.py
+++ src/ethereum_clis/clis/ethrex.py
@@ -7,18 +7,11 @@ class EthrexExceptionMapper(ExceptionMapper):
     """Ethrex exception mapper."""
 
     mapping_substring = {
-        TransactionException.SENDER_NOT_EOA: (
-            "reject transactions from senders with deployed code"
-        ),
         TransactionException.INSUFFICIENT_MAX_FEE_PER_GAS: "gas price is less than basefee",
-        TransactionException.INSUFFICIENT_MAX_FEE_PER_BLOB_GAS: (
-            "blob gas price is greater than max fee per blob gas"
-        ),
         TransactionException.PRIORITY_GREATER_THAN_MAX_FEE_PER_GAS: (
             "priority fee is greater than max fee"
         ),
         TransactionException.TYPE_3_TX_MAX_BLOB_GAS_ALLOWANCE_EXCEEDED: "Exceeded MAX_BLOB_GAS_PER_BLOCK",
-        TransactionException.TYPE_3_TX_INVALID_BLOB_VERSIONED_HASH: "blob version not supported",
         TransactionException.TYPE_4_EMPTY_AUTHORIZATION_LIST: "empty authorization list",
         TransactionException.TYPE_4_TX_CONTRACT_CREATION: "unexpected length",
         TransactionException.TYPE_4_TX_PRE_FORK: (
@@ -26,9 +19,6 @@ class EthrexExceptionMapper(ExceptionMapper):
         ),
         TransactionException.INVALID_DEPOSIT_EVENT_LAYOUT: (
             "failed to decode deposit requests from receipts"
-        ),
-        TransactionException.TYPE_3_TX_PRE_FORK: (
-            "blob versioned hashes not supported"
         ),
         TransactionException.TYPE_3_TX_INVALID_BLOB_VERSIONED_HASH: (
             "blob version not supported"
@@ -40,18 +30,28 @@ class EthrexExceptionMapper(ExceptionMapper):
         BlockException.INCORRECT_BLOB_GAS_USED: "Blob gas used doesn't match value in header"
     }
     mapping_regex = {
+        TransactionException.SENDER_NOT_EOA: (
+            r"reject transactions from senders with deployed code|Sender account should not have bytecode"
+        ),
         TransactionException.NONCE_MISMATCH_TOO_LOW: r"nonce \d+ too low, expected \d+",
         TransactionException.TYPE_3_TX_MAX_BLOB_GAS_ALLOWANCE_EXCEEDED: (
             r"blob gas used \d+ exceeds maximum allowance \d+"
         ),
         TransactionException.TYPE_3_TX_ZERO_BLOBS: (
-            r"blob transactions present in pre-cancun payload|empty blobs"
+            r"blob transactions present in pre-cancun payload|empty blobs|Type3TxZeroBlobs"
+        ),
+        TransactionException.TYPE_3_TX_INVALID_BLOB_VERSIONED_HASH: r"blob version not supported|Type3TxInvalidBlobVersionedHash",
+        TransactionException.TYPE_3_TX_PRE_FORK: (
+            r"blob versioned hashes not supported|Type 3 transactions are not supported before the Cancun fork"
         ),
         TransactionException.INSUFFICIENT_ACCOUNT_FUNDS: (
             r"lack of funds \(\d+\) for max fee \(\d+\)|Insufficient account founds"
         ),
         TransactionException.INTRINSIC_GAS_TOO_LOW: (
             r"gas floor exceeds the gas limit|call gas cost exceeds the gas limit|Intrinsic gas too low"
+        ),
+        TransactionException.INSUFFICIENT_MAX_FEE_PER_BLOB_GAS: (
+            r"blob gas price is greater than max fee per blob gas|Insufficient max fee per blob gas"
         ),
         TransactionException.INITCODE_SIZE_EXCEEDED: (
             r"create initcode size limit|Initcode size exceeded"
